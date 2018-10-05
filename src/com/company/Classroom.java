dd39b34419256e884ef55808e271b9776b215107
BT_1N3_23@bktech.local <BT_1N3_23@bktech.local>
2018-10-05 11:39:03 -0400

    Initial commit

---
+++ src/com/company/Classroom.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Classroom extends Person {
+
+    private String student[];
+    private String teacher;
+
+    public Classroom()
+    {
+        super()
+        this.student = student;
+        this.teacher = teacher;
+    }
+
+    public classAverage()
+    {
+        for (int i = 0; i <= student[].length(); i++)
+        {
+            GPA += student[i].GPA;
+            double AVG = GPA / student[].length();
+        }
+    }
+
+    public printClass()
+    {
+        String sClass = "";
+        sClass += teacher;
+        for (int h = o; h <= student[].length; h++)
+        {
+            sClass += ", " + student[h];
+        }
+    }
+}
---
+++ src/com/company/Runner.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Runner {
+
+    static String[] firstNames = {"Arthur, Albert, Arnold, Bernardo, Christian, Christopher, Dennis, Dent, Dover, Derek," +
+            "Eric, Finn, Gunther, Herbert, Jack, John, Jason, Jerry, Joshua, Keith"};
+    static String[] familynames = {"Arden, Arrow, Art, Brown, Brinn, Conner, Dean, Eden, Fan, Hill, Hull, Grey"};
+
+    public static void main(String[] args)
+    {
+
+    }
+}
